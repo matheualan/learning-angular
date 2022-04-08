e16991754b3d4cc1f8ca7a5c535724c29d3c6f1f
Matheus Alan <matheusalanpv@outlook.com>
2022-04-07 22:07:35 -0300

    Aula Renderização condicional

---
+++ src/app/components/if-render/if-render.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  templateUrl: './if-render.component.html',
+  styleUrls: ['./if-render.component.css']
+})
+export class IfRenderComponent implements OnInit {
+  canShow: boolean = false; //usado com o *ngIf=canShow para liberar ou não algum conteúdo
+  
+  name: string = 'Matheus';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
